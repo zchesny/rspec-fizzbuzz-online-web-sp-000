455a836509da56d56e5d5f580fc7bb53256338f1
Zoey Chesny <z.chesny@gmail.com>
2020-04-07 21:32:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num) 
+def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
     return "FizzBuzz"
   elsif num % 3 == 0
